b0aa9df45918e85e98b42d3853d8b1b5e44441c4
Sanheen Sethi <sanheensethi37659@gmail.com>
2022-01-10 02:11:27 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,63 @@
+function getId(id){
+	return document.getElementById(id);
+}
+
+function getClass(className){
+	return document.getElementsByClassName(className);
+}
+
+let clear = getId("clear");
+let getAllBtns = getClass("but");
+let onoff = getId("onoff");
+let display = getId("display");
+
+let allOff = false;
+for(btn of getAllBtns){
+	btn.disabled = true;
+	allOff = true;
+}
+
+onoff.addEventListener('click',function(){
+	if(allOff){
+		for(btn of getAllBtns){
+			btn.disabled = false;
+			allOff = false;
+		}
+		onoff.innerHTML = "ON"
+		display.innerHTML = "0"
+		onoff.classList.remove("bg-danger");
+		onoff.classList.add("bg-success");		
+	}else{
+		for(btn of getAllBtns){
+			btn.disabled = true;
+			allOff = true;
+		}
+		onoff.innerHTML = "OFF"
+		display.innerHTML = ""
+		onoff.classList.remove("bg-success");
+		onoff.classList.add("bg-danger");
+	}
+});
+
+evalString = "";
+
+clear.addEventListener('click',function(){
+	evalString = "";
+	display.innerHTML = "0"
+})
+
+for(let i = 0 ; i < getAllBtns.length;i++){
+	let btn = i;
+	if(getAllBtns[btn].attributes.data.nodeValue == "="){
+		getAllBtns[btn].addEventListener('click',function(){
+			let ans = eval(evalString);
+			display.innerHTML = ans;
+		})
+		continue;
+	}
+	getAllBtns[btn].addEventListener('click',function(){
+		evalString += getAllBtns[btn].attributes.data.nodeValue;
+		display.innerHTML = evalString;
+	});
+}
+
